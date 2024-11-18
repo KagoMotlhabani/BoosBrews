142dc6c6f8a6913a1624b120d245e915fe914fb6
Kago Motlhabani <kagomw@gmail.com>
2024-11-19 00:14:57 +0200

    Background Music Added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip backgroundMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = backgroundMusic;
+        audioSource.Play();
+        
+    }
+
+
+}
